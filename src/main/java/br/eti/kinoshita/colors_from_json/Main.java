7cc95832cb3d7a5e45b9008fa219d2cadffa8765
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2021-12-13 13:07:33 +1300

    Use java.awt, simplify parsing

--- src/main/java/br/eti/kinoshita/colors_from_json/Main.java
+++ src/main/java/br/eti/kinoshita/colors_from_json/Main.java
@@ -1,5 +1,6 @@
 package br.eti.kinoshita.colors_from_json;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -16,6 +17,81 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main {
 
+    /**
+     * For convenience, creates an HTML code given a Java awt color.
+     * @param color
+     * @return
+     */
+    private static String toHtmlCode(String color) {
+        final Color colorObject = new Color(Integer.parseInt(color), /* hasAlpha? */ true);
+        String red = Integer.toHexString(colorObject.getRed());
+        String green = Integer.toHexString(colorObject.getGreen());
+        String blue = Integer.toHexString(colorObject.getBlue());
+
+        return "#" +
+                (red.length() == 1? "0" + red : red) +
+                (green.length() == 1? "0" + green : green) +
+                (blue.length() == 1? "0" + blue : blue);
+    }
+
+    public static void main(String[] args) throws IOException, DecoderException {
+        File jsonFile = new File(Main.class.getResource("/MAP.json")
+                .getFile());
+        String jsonInput = FileUtils.readFileToString(jsonFile, Charset.defaultCharset());
+        ObjectMapper mapper = new ObjectMapper();
+        List<Entry> entries = mapper.readValue(jsonInput, mapper.getTypeFactory()
+                .constructCollectionType(List.class, Entry.class));
+        try (FileWriter fos = new FileWriter(new File("/tmp/report.html"))) {
+            fos.write("<table border='1' width='100%'>"
+                    + "<thead>"
+                    + "<tr>"
+                    + "<th>Shapefile</th><th>Type</th><th>Color</th><th>Image</th><th>Label color</th>"
+                    + "</tr>"
+                    + "</thead>"
+                    + "<tbody>");
+            for (Entry entry : entries) {
+                String shapeFile = entry.getFileName();
+                String type = entry.getCategory() == null ? "" : entry.getCategory().split("\\|")[0];
+                String categoryColorTd = "<td></td>";
+                if (type != null && !type.equals("")) {
+                    if (!type.equals("User defined")) {
+                        final String htmlColor = toHtmlCode(entry.getCategory().split("\\|")[1]);
+                        // categoryColor = normalizeColor(categoryColor, entry.isExclusionZone != 0);
+                        categoryColorTd = "<td style='background: " + htmlColor + "'>" + htmlColor + "</td>";
+                    } else {
+                        final String[] categoryTokens = entry.getCategory().split("\\|");
+
+                        String firstColorHtml = toHtmlCode(categoryTokens[1]);
+                        String secondColorHtml = toHtmlCode(categoryTokens[2]);
+                        String thirdColorHtml = toHtmlCode(categoryTokens[3]);
+
+                        categoryColorTd = "<td><table border='0' width='100%'>"
+                                + "<tr>"
+                                + "<td width='50%' style='background: " + firstColorHtml + "'>" + firstColorHtml + "</td>"
+                                + "<td width='50%' style='background: " + secondColorHtml + "'>" + secondColorHtml + "</td>"
+                                + "<td width='50%' style='background: " + thirdColorHtml + "'>" + thirdColorHtml + "</td>"
+                                + "</tr>"
+                                + "</table></td>";
+                    }
+                }
+                String png = entry.getPattern() != null ? Base64.encodeBase64String(Hex.decodeHex(entry.getPattern().replaceAll("'",  ""))) : "";
+                String labelColor = entry.getLabelColor() == null ? "" : Integer.toHexString(entry.getLabelColor());
+                fos.write("<tr>"
+                        + "<td>" + shapeFile + "</td>"
+                        + "<td>" + type + "</td>"
+                        + categoryColorTd
+                        + "<td><img src='data:image/png;base64," + png + "' /></td>"
+                        + "<td style='background: #" + labelColor + "'>#" + labelColor + "</td>"
+                        + "</tr>");
+            }
+            fos.write("</tbody>"
+                    + "</table>");
+        }
+    }
+
+    /**
+     * Used to parse the JSON value.
+     */
     public static class Entry {
         @JsonAlias("FileName")
         private String fileName;
@@ -223,84 +299,4 @@ public class Main {
 
     }
 
-    public static String normalizeColor (String s, boolean exclusionZone) {
-        if (exclusionZone) {
-            // s.length() == 6
-            // return normalizeColor(s + "00");
-            // No idea about this one, honestly. The best guess I could make is that exclusion zones are red?
-            return "FF0000FF";
-        }
-        // an eight length color has the alpha color at the beginning, whereas it must be at the end for browsers?
-        if (s.length() == 8) {
-            return s.substring(2) + s.substring(0, 2);
-        }
-        // a two length color appears to be missing left padding zeroes, and let's throwing in the alpha channel to have everything with 8 digit?
-        if (s.length() == 2) {
-            return "0000" + s + "FF";
-        }
-        return s;
-    }
-    
-    public static void main(String[] args) throws IOException, DecoderException {
-        File jsonFile = new File(Main.class.getResource("/MAP.json")
-                .getFile());
-        String jsonInput = FileUtils.readFileToString(jsonFile, Charset.defaultCharset());
-        ObjectMapper mapper = new ObjectMapper();
-        List<Entry> entries = mapper.readValue(jsonInput, mapper.getTypeFactory()
-                .constructCollectionType(List.class, Entry.class));
-        try (FileWriter fos = new FileWriter(new File("/tmp/report.html"))) {
-            fos.write("<table border='1' width='100%'>"
-                    + "<thead>"
-                    + "<tr>"
-                    + "<th>Shapefile</th><th>Type</th><th>Color</th><th>Image</th><th>Label color</th>"
-                    + "</tr>"
-                    + "</thead>"
-                    + "<tbody>");
-            for (Entry entry : entries) {
-                String shapeFile = entry.getFileName();
-                String type = entry.getCategory() == null ? "" : entry.getCategory().split("\\|")[0];
-                String categoryColor = entry.getCategory() == null ? "" : Integer.toHexString(Integer.parseInt(entry.getCategory().split("\\|")[1]));
-                String categoryColorTd = "<td></td>";
-                if (type != null && !type.equals("")) {
-                    if (!type.equals("User defined")) {
-                        // This is strange... in a 8 digit hex, I think the transparency was set by the last two digits... that's
-                        // how the browser interprets it too. But here it looks like if we move the two first digits to the end
-                        // then we get the right color/transparency? Except not for User defined?
-                        categoryColor = normalizeColor(categoryColor, entry.isExclusionZone != 0);
-                        categoryColorTd = "<td style='background: #" + categoryColor + "'>#" + categoryColor + "</td>";
-                    } else {
-                        // Now comes to interesting part... the Category contains three numbers. The image contains two
-                        // images. Well, we can try the following:
-                        // 1. Assume the first number is the base color, not used.
-                        // 2. And adding up the first and the second color, now we get a derived color. Used in the first part of the image.
-                        // 3. You guessed right!
-                        int baseColor = Integer.parseInt(entry.getCategory().split("\\|")[1]);
-                        int secondColor = Integer.parseInt(entry.getCategory().split("\\|")[2]);
-                        int thirdColor = Integer.parseInt(entry.getCategory().split("\\|")[3]);
-                        
-                        String leftColor = normalizeColor(Integer.toHexString(baseColor + secondColor + thirdColor), entry.isExclusionZone != 0);
-                        String rightColor = normalizeColor(Integer.toHexString(baseColor + secondColor), entry.isExclusionZone != 0);
-                        
-                        categoryColorTd = "<td><table border='0' width='100%'>"
-                                + "<tr>"
-                                + "<td width='50%' style='background: #" + leftColor + "'>#" + leftColor + "</td>"
-                                + "<td width='50%' style='background: #" + rightColor + "'>#" + rightColor + "</td>"
-                                + "</tr>"
-                                + "</table></td>";
-                    }
-                }
-                String png = entry.getPattern() != null ? Base64.encodeBase64String(Hex.decodeHex(entry.getPattern().replaceAll("'",  ""))) : "";
-                String labelColor = entry.getLabelColor() == null ? "" : Integer.toHexString(entry.getLabelColor());
-                fos.write("<tr>"
-                        + "<td>" + shapeFile + "</td>"
-                        + "<td>" + type + "</td>"
-                        + categoryColorTd
-                        + "<td><img src='data:image/png;base64," + png + "' /></td>"
-                        + "<td style='background: #" + labelColor + "'>#" + labelColor + "</td>"
-                        + "</tr>");
-            }
-            fos.write("</tbody>"
-                    + "</table>");
-        }
-    }
 }
